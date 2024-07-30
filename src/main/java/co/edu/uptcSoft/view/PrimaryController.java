a53a5667fc90257072215ad7cb2ee207c114026a
DaniGuevara09 <laura.guevara02@uptc.edu.co>
2024-07-30 10:45:30 -0500

    Addition of javaFx and Maven to the project

---
+++ src/main/java/co/edu/uptcSoft/view/PrimaryController.java
@@ -0,0 +1,13 @@
+package co.edu.uptcSoft.view;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ src/main/java/co/edu/uptcSoft/view/SecondaryController.java
@@ -0,0 +1,13 @@
+package co.edu.uptcSoft.view;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
